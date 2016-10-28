7e3b3716ff3c90089f463d44667803957d917a3e
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-10-28 17:13:30 +0100

    Working around WinRM::WinRMAuthorizationError on appveyor...

--- winrm/spec_helper.rb
+++ winrm/spec_helper.rb
@@ -7,6 +7,8 @@ opts = {
   endpoint: 'http://127.0.0.1:5985/wsman',
   user: 'appveyor',
   password: ENV['WINDOWS_PASSWORD'],
-  operation_timeout: 300 # 5 minutes max timeout for any operation
+  operation_timeout: 300, # 5 minutes max timeout for any operation
+  transport: :plaintext,
+  basic_auth_only: true
 }
 Specinfra.configuration.winrm = WinRM::Connection.new(opts)
